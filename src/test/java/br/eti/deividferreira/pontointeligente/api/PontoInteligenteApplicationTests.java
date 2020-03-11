0ee354dbd2adce72434af1e8ae3c951bec21dab9
Deivid Ferreira <deividpse@gmail.com>
2020-03-11 20:00:58 -0300

    initial commit

---
+++ src/test/java/br/eti/deividferreira/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.deividferreira.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
